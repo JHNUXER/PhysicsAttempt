34013368537404e1fc0a670df26506bca4ba46e6
John Kayne <johnhoganuke2@gmail.com>
2017-10-04 14:20:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ Game.objects = [];
 Game.version = "v0.0.0ppa";
 
 Game.tick = function() {}
+
+class PhysicsObject {
+  constructor(domElem,mass,position) {
+    this.domElem = domElem;
+    this.mass = mass;
+    this.position = position;
+    this.murderKreshan = function() {
+      console.log("IT IS DONE COMRADE!!!! THE TRAITOR KRESHAWN IS DEAD!!!");
+    }
+  }
+}
